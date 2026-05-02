6fbfee6fa6ce8d0d87c17467533e4becf6dcdcfc
Mailoud <moha2252003@gmail.com>
2026-05-02 21:23:23 +0200

    mmkj

---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    // Trust ALL proxies (safe for DigitalOcean)
+    protected $proxies = '*';
+
+    protected $headers = Request::HEADER_X_FORWARDED_FOR |
+                         Request::HEADER_X_FORWARDED_HOST |
+                         Request::HEADER_X_FORWARDED_PORT |
+                         Request::HEADER_X_FORWARDED_PROTO;
+}
